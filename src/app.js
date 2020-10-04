4002791a492928bee39d44b3a6c7e35011342eca
sangin <sangin84@gmail.com>
2020-10-05 08:06:12 +0900

    update

--- src/app.js
+++ src/app.js
@@ -12,15 +12,15 @@ import 'react-dates/lib/css/_datepicker.css';
 
 const store = configureStore();
 
-store.dispatch(
-  addExpense({ description: 'Water bill', amount: 4500, createdAt: 1000 })
-);
-store.dispatch(
-  addExpense({ description: 'Gas bill', amount: 1000, createdAt: 2000 })
-);
-store.dispatch(
-  addExpense({ description: 'Rent', amount: 109500, createdAt: 3000 })
-);
+// store.dispatch(
+//   addExpense({ description: 'Water bill', amount: 4500, createdAt: 1000 })
+// );
+// store.dispatch(
+//   addExpense({ description: 'Gas bill', amount: 1000, createdAt: 2000 })
+// );
+// store.dispatch(
+//   addExpense({ description: 'Rent', amount: 109500, createdAt: 3000 })
+// );
 
 // store.dispatch(setTextFilter("water"));
 
