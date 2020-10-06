4cc6fe361fbae833e31adeb3702b6ef6f066e4ba
sangin <sangin84@gmail.com>
2020-10-07 05:49:42 +0900

    update

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 import moment from "moment";
+import numeral from "numeral";
 
 export const ExpenseListItem = ({
   dispatch,
@@ -16,7 +17,8 @@ export const ExpenseListItem = ({
     </Link>
 
     <p>
-      {amount} - {moment(createdAt).format("MMMM Do, YYYY")}
+      {numeral(amount / 100).format("$0,0.00")} -{" "}
+      {moment(createdAt).format("MMMM Do, YYYY")}
     </p>
 
     <br></br>
